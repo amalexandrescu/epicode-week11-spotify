52b0d4bd119f1faf16ab3a73632710fd5886b053
Alexis <alexandrescu.alexandra.maria@gmail.com>
2022-12-11 21:34:34 +0200

    redux persist installed

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -3,6 +3,10 @@ import recentlyPlayedReducer from "../reducers/recentlyPlayedReducer";
 import mainSearchReducer from "../reducers/mainSearchReducer";
 import likedSongsReducer from "../reducers/likedSongsReducer";
 import goodMorningReducer from "../reducers/goodMorningReducer";
+import localStorage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+import { encryptTransform } from "redux-persist-transform-encrypt";
 // configureStore will set up the Redux Store for us!
 
 const bigReducer = combineReducers({
@@ -13,11 +17,31 @@ const bigReducer = combineReducers({
   // currentSearchFetchedSongs: mainSearchReducer,
 });
 
-const store = configureStore({
-  reducer: bigReducer,
+const persistConfig = {
+  key: "root", // ???
+  storage: localStorage, // the default engine
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_SECRET_KEY,
+    }),
+  ],
+};
+
+const persistedReducer = persistReducer(persistConfig, bigReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer, // here there's place for just 1 value!
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+      // this is for shutting down the serializable check
+      // performed by Redux, and getting rid of the error in the console
+    }),
 });
 
-export default store;
+export const persistor = persistStore(store);
+
+// export default store;
 
 // now the store is ready! let's INJECT IT into our REACT APP!
 // we do it in the src/index.js file
